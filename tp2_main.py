b7924fd6a9b8c76f2dc76b5463f9abff91fe02c0
Fernando Davila <a91403@ualg.pt>
2026-03-08 15:51:57 +0000

    Imported colorama and Tested stock.comprar

--- tp2_main.py
+++ tp2_main.py
@@ -1,5 +1,16 @@
 from gestor_stock import GestorStock
 from graph import show_graph
+from colorama import Fore, Style,init
+init()
 
 def main():
-    pass
+    
+    stock= GestorStock("AAPL","Apple",130,8)
+    print(stock)
+
+    stock.comprar(10,100)
+    print(f"{Fore.RED}\nAfter buying: ")
+    print(stock)
+
+if __name__ == "__main__":
+    main()
