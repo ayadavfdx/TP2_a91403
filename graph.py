876bfbe8297cb3f27e39d7827b24821b4b704e3d
Fernando Davila <a91403@ualg.pt>
2026-03-08 15:02:51 +0000

    Added graph of stock price

--- graph.py
+++ graph.py
@@ -4,4 +4,13 @@ import plotly.graph_objects as go
 def show_graph(symbol: str):
     data = yf.download(symbol)
 
-    
+    figure= go.Figure()
+
+    figure.add_trace(
+        go.Scatter(
+            x=data.index,
+            y=data["Close"],
+            mode="lines",
+            name=symbol
+        )
+    )
